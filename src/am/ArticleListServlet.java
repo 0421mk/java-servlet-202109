6c5656b47576a2f66f07ef6063a87a6f458090fd
0421mk@naver.com <0421mk@naver.com>
2021-09-20 17:46:20 +0900

    DB 연결

---
+++ src/am/ArticleListServlet.java
@@ -0,0 +1,72 @@
+package am;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import am.util.SecSql;
+
+import am.util.DBUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ArticleListServlet
+ */
+@WebServlet("/article/list")
+public class ArticleListServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html charset=UTF-8");
+		String url = "jdbc:mysql://localhost:3306/am?ServerTimeZone=UTC";
+		String user = "root";
+		String password = "";
+
+		// 커넥터 드라이버 활성화
+		String driverName = "com.mysql.cj.jdbc.Driver";
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+
+			DBUtil dbUtil = new DBUtil(request, response);
+
+			SecSql sql = new SecSql();
+
+			sql.append("SELECT * FROM article");
+			List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql);
+
+			response.getWriter().append(articleRows.toString());
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
